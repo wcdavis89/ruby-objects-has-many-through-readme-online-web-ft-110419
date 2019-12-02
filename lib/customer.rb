ebfbbfdfaa4ec8f88e0e36941cbeb7f1756d80bf
Whittney Davis <wcdavis89@yahoo.com>
2019-12-02 03:49:43 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
+  attr_accessor :name, :age 
+  @@all=[]
+  def initialize(name, age)
+    @name=name
+    @age=age
+    @@all << self
+  end
   
+  def new_meal(waiter,total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  def self.all
+    @@all
+  end 
 end
